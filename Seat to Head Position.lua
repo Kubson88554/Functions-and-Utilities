5cc3107c3f7d9b2edc9043d8f7224a4208324cdd
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-14 04:06:58 -0300

    Update Seat to Head Position.lua

--- Seat to Head Position.lua
+++ Seat to Head Position.lua
@@ -1,7 +1,8 @@
+--By Jumper#4155, shortened and adapted to work with my library
 clamp = function(x,s,l) return x < s and s or x > l and l or x end
 
 headazim = clamp(lookX, -0.277, 0.277) * 0.408 * pi2
-headelev =  clamp(lookY, -0.125, 0.125) * 0.9 * pi2 + 0.404 + math.abs(headazim/0.7101) * 0.122
+headelev =  clamp(lookY, -0.125, 0.125) * 0.9 * pi2 + 0.404 + m.abs(headazim/0.7101) * 0.122
  
-distance = math.cos(headazim) * 0.1523
-headpos = vec(math.sin(headazim)*0.1523, math.cos(headelev)*distance-0.839, math.sin(headelev)*distance-0.023)
+distance = m.cos(headazim) * 0.1523
+headpos = vec(m.sin(headazim)*0.1523, m.cos(headelev)*distance-0.839, m.sin(headelev)*distance-0.023)
