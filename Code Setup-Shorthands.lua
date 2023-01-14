d94fcb1a6f66e0750c7ba158450652f1304450bb
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-14 14:12:05 -0300

    Update Code Setup-Shorthands.lua

--- Code Setup-Shorthands.lua
+++ Code Setup-Shorthands.lua
@@ -1,2 +1,2 @@
 ign = input.getNumber; osn = output.setNumber; igb = input.getBool; osb = output.setBool;
-pgn = property.getNumber; pgb = property.getBool; m = math; pi = m.pi; pi2 = pi*2
+pgn = property.getNumber; pgb = property.getBool; pgt = property.getText; m = math; pi = m.pi; pi2 = pi*2
