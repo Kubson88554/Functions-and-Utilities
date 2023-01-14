9657dc9345ebaa075aa01db28d32d415a7975fbf
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-14 14:34:08 -0300

    Update Vector Functions.lua

--- Vector Functions.lua
+++ Vector Functions.lua
@@ -60,6 +60,9 @@ local ab=subt(b,a)
 local t=(-(d+dot(normal,a)))/dot(normal,ab)
 return vec(a.x+ab.x*t,a.y+ab.y*t,a.z+ab.z*t)
 end
+function debugvec(a)
+debug.log("VECTOR COMPONENTS:")debug.log(a.x)debug.log(a.y)debug.log(a.z)debug.log("------------------")
+end
 
 --minified
 m=math;function vec(a,b,c)return{x=a or 0,y=b or 0,z=c or 0}end;function add(d,e)return vec(d.x+e.x,d.y+e.y,d.z+e.z)end;function mult(d,e)return vec(d.x*e.x,d.y*e.y,d.z*e.z)end;function multf(d,f)return vec(d.x*f,d.y*f,d.z*f)end;function invert(d)return multf(d,-1)end;function subt(d,e)return add(d,invert(e))end;function length(d)return m.sqrt(d.x*d.x+d.y*d.y+d.z*d.z)end;function divf(d,f)return multf(d,1/f)end;function norm(d)return divf(d,length(d))end;function dot(d,e)return d.x*e.x+d.y*e.y+d.z*e.z end;function cross(d,e)return vec(d.y*e.z-d.z*e.y,d.z*e.x-d.x*e.z,d.x*e.y-d.y*e.x)end;function reject(d,e)return subt(d,multf(norm(e),dot(d,norm(e))))end;function reflect(d,e,g)return subt(d,multf(reject(d,e),g or 2))end;function stoc(h,i,j)local j=j or 1;return vec(m.sin(h)*m.cos(i)*j,m.cos(h)*m.cos(i)*j,m.sin(i)*j)end;function tolocal(d,k,l,n)return vec(dot(k,d),dot(l,d),dot(n,d))end;function torelative(d,k,l,n)return add(add(multf(k,d.x),multf(l,d.y)),multf(n,d.z))end;function tospherical(d)return length(d),m.atan(d.y,d.x),m.asin(d.z/length(d))end;function p3toplane(d,e,o)local p=cross(subt(o,d),subt(e,d))return p,dot(p,invert(d))end;function intersect(d,e,p,j)local q=subt(e,d)local r=-(j+dot(p,d))/dot(p,q)return vec(d.x+q.x*r,d.y+q.y*r,d.z+q.z*r)end
