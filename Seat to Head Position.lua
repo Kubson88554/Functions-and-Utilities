41c441b9e841614187cfa944bf5a959354cc6f2a
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-14 12:39:29 -0300

    Update Seat to Head Position.lua

--- Seat to Head Position.lua
+++ Seat to Head Position.lua
@@ -5,4 +5,4 @@ headazim = clamp(lookX, -0.277, 0.277) * 0.408 * pi2
 headelev =  clamp(lookY, -0.125, 0.125) * 0.9 * pi2 + 0.404 + m.abs(headazim/0.7101) * 0.122
  
 distance = m.cos(headazim) * 0.1523
-headpos = vec(m.sin(headazim)*0.1523, m.cos(headelev)*distance-0.839, m.sin(headelev)*distance-0.023)
+headpos = vec(m.sin(headazim)*0.1523, m.cos(headelev)*distance+0.061, m.sin(headelev)*distance-0.023)
