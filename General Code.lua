5f935ae1232640c691fb1d90759f513cd60dd3e7
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-15 14:57:42 -0300

    Update General Code.lua

--- General Code.lua
+++ General Code.lua
@@ -1,75 +1,84 @@
 ign = input.getNumber; osn = output.setNumber; igb = input.getBool; osb = output.setBool;
 pgn = property.getNumber; pgb = property.getBool; pgt = property.getText; m = math; pi = m.pi; pi2 = pi*2
 
-function vec(x,y,z)
+function vec(x,y,z) --defines a vector
 return {x=x or 0,y=y or 0,z=z or 0}
 end
-function add(a,b)
+function add(a,b) --adds 2 vectors
 return vec(a.x+b.x, a.y+b.y, a.z+b.z)
 end
-function mult(a,b)
+function mult(a,b) --multiplies 2 vectors
 return vec(a.x*b.x, a.y*b.y, a.z*b.z)
 end
-function multf(a,n)
+function multf(a,n) --multiplies a vector by a factor
 return vec(a.x*n, a.y*n, a.z*n)
 end
-function invert(a)
+function invert(a) --inverts a vector
 return multf(a,-1)
 end
-function subt(a,b)
+function subt(a,b) --subtracts a vector from another
 return add(a,invert(b))
 end
-function length(a)
+function length(a) --gets length of a vector
 return m.sqrt(a.x*a.x+a.y*a.y+a.z*a.z)
 end
-function divf(a,n)
+function divf(a,n) --divides vector by a factor
 return multf(a,1/n)
 end
-function norm(a)
+function norm(a) --normalizes a vector
 return divf(a,length(a))
 end
-function dot(a,b)
+function dot(a,b) --dot product between 2 vectors
 return a.x*b.x+a.y*b.y+a.z*b.z
 end
-function cross(a,b)
+function cross(a,b) --cross product between 2 vectors
 return vec(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x)
 end
-function reject(a,b)
+function reject(a,b) --rejects a vector by another
 return subt(a, multf(norm(b), dot(a, norm(b))))
 end
-function reflect(a, b, factor)
+function reflect(a,b,factor) --reflects a vector across another
 return subt(a, multf(reject(a, b), factor or 2))
 end
-function stoc(hor,ver,d)
+function stoc(hor,ver,d) --spherical to cartesian conversion
 local d=d or 1
 return vec(m.sin(hor)*m.cos(ver)*d, m.cos(hor)*m.cos(ver)*d, m.sin(ver)*d)
 end
-function tolocal(a,r,f,u)
+function tolocal(a,r,f,u) --converts a vector to local frame of reference
 return vec(dot(r,a),dot(f,a),dot(u,a))
 end
-function torelative(a,r,f,u)
+function torelative(a,r,f,u) --converts a vector to relative frame of reference
 return add(add(multf(r,a.x), multf(f,a.y)), multf(u,a.z))
 end
-function tospherical(a)
+function tospherical(a) --cartesian to spherical conversion
 return length(a),m.atan(a.y,a.x),m.asin(a.z/length(a))
 end
-function p3toplane(a,b,c)
-local normal=cross(subt(c,a),subt(b,a))
-return normal,dot(normal,invert(a))
+function toplane(p1,p2,p3) --converts 3 points into a normal vector and distance (plane)
+local normal=cross(norm(subt(p3,p1)),norm(subt(p2,p1)))
+return normal,dot(normal,invert(p1))
 end
-function intersect(a,b,normal,d)
-local ab=subt(b,a)
-local t=(-(d+dot(normal,a)))/dot(normal,ab)
-return vec(a.x+ab.x*t,a.y+ab.y*t,a.z+ab.z*t)
+function intersect(p1,p2,normal,d) --calculates intersection point of line made between 2 vectors through a plane
+local ab=subt(p2,p1)
+local t=(-(d+dot(normal,p1)))/dot(normal,ab)
+return vec(p1.x+ab.x*t,p1.y+ab.y*t,p1.z+ab.z*t)
 end
-function clamp(x,s,l) return x < s and s or x > l and l or x end
-function seatheadpos(lookX,lookY,headorigin)
-local azimuth = clamp(lookX, -0.277, 0.277) * 0.408 * pi2
-local elevation =  clamp(lookY, -0.125, 0.125) * 0.9 * pi2 + 0.404 + m.abs(azimuth/0.7101) * 0.122
-local distance = m.cos(azimuth) * 0.1523
-return vec(m.sin(azimuth)*0.1523, m.cos(elevation)*distance+0.096, m.sin(elevation)*distance-0.023)
-function debugvec(a)
-debug.log("VECTOR COMPONENTS:")debug.log(a.x)debug.log(a.y)debug.log(a.z)debug.log("------------------")
+function clamp(x,s,l) return x < s and s or x > l and l or x --clamp (used for seat to head position function)
+end
+function seatheadpos(lookX,lookY,headorigin) --calculates head position
+local azimuth=clamp(lookX,-0.277,0.277)*0.408*pi2
+local elevation=clamp(lookY,-0.125,0.125)*0.9*pi2+0.404+m.abs(azimuth/0.7101)*0.122
+local distance = m.cos(azimuth)*0.1523
+return vec(m.sin(azimuth)*0.1523,m.cos(elevation)*distance+0.096,m.sin(elevation)*distance-0.023)
+end
+function debugvec(v,name) --sends components of a vector to debug
+debug.log("VECTOR ".. name .." COMPONENTS:")debug.log(v.x)debug.log(v.y)debug.log(v.z)debug.log("------------------")
+end
+function toHUD(HUDp1,HUDp2,HUDp3,p1,p2) --calculates intersection point display on HUD
+local HUDfwd,HUDright=norm(subt(HUDp2,HUDp1)),norm(subt(HUDp3,HUDp1))
+local HUDnormal=cross(HUDright,HUDfwd)
+local d=dot(HUDnormal,invert(HUDp1))
+local intsct = intersect(p1,p2,HUDnormal,d)
+return multf(tolocal(subt(intsct,HUDp1),HUDright,HUDfwd,HUDnormal),128)
 end
 
 --compass and tilt to facing vectors
