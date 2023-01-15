418eee5cc96d125481ee998d5577270879184998
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2023-01-15 20:33:38 -0300

    Update Seat to Head Position.lua

--- Seat to Head Position.lua
+++ Seat to Head Position.lua
@@ -5,3 +5,4 @@ local azimuth = clamp(lookX, -0.277, 0.277) * 0.408 * pi2
 local elevation =  clamp(lookY, -0.125, 0.125) * 0.9 * pi2 + 0.404 + m.abs(azimuth/0.7101) * 0.122
 local distance = m.cos(azimuth) * 0.1523
 return vec(m.sin(azimuth)*0.1523, m.cos(elevation)*distance+0.096, m.sin(elevation)*distance-0.023)
+end
