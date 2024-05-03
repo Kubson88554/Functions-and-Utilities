b10821f5917ffd3caad44591e2de681e9969b7e1
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2024-05-03 00:51:26 -0300

    Update Code Setup-Shorthands.lua

--- Code Setup-Shorthands.lua
+++ Code Setup-Shorthands.lua
@@ -1,11 +1,2 @@
-ign = input.getNumber; osn = output.setNumber; igb = input.getBool; osb = output.setBool;
-pgn = property.getNumber; pgb = property.getBool; pgt = property.getText; m = math; pi = m.pi; pi2 = pi*2
-
-rx,ry,rz=ign(4),ign(5),ign(6)
-
-cx,cy,cz=cos(rx),cos(ry),cos(rz)
-sx,sy,sz=sin(rx),sin(ry),sin(rz)
-
-right = vec(cy*cz,-sy,cy*sz)
-fwd = vec(sx*sz+cx*sy*cz,cx*cy,-sx*cz+cx*sy*sz)
-up = cross(right,fwd)
+ign = input.getNumber; osn = output.setNumber; igb = input.getBool; osb = output.setBool; pgn = property.getNumber; pgb = property.getBool; pgt = property.getText;
+m = math; pi = m.pi; pi2 = pi*2; cos = m.cos; acos = m.acos; sin = m.sin; asin = m.asin; tan = m.tan; atan = m.atan; sqrt = m.sqrt; abs = m.abs
