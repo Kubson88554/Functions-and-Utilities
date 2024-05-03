96d5ea8faf3c87d18da20c1653583e21f79a1e15
Kubson88554 <75774233+Kubson88554@users.noreply.github.com>
2024-05-03 00:53:02 -0300

    Update Kubs Library V2.lua

--- Kubs Library V2.lua
+++ Kubs Library V2.lua
@@ -1,3 +1,6 @@
+ign = input.getNumber; osn = output.setNumber; igb = input.getBool; osb = output.setBool; pgn = property.getNumber; pgb = property.getBool; pgt = property.getText;
+m = math; pi = m.pi; pi2 = pi*2; cos = m.cos; acos = m.acos; sin = m.sin; asin = m.asin; tan = m.tan; atan = m.atan; sqrt = m.sqrt; abs = m.abs
+
 --Basic Vector Functions
 
 -- VectorNew
